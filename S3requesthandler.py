15113da20373698fe24ca420bb2869f8202c5b9d
10sajan10 <sajanneupane137@gmail.com>
2024-11-19 01:44:34 -0700

    test on sample data

--- S3requesthandler.py
+++ S3requesthandler.py
@@ -6,7 +6,7 @@ from SQSRequestReceiver import SQSRequestReceiver
 import logging
 
 class S3RequestHandler:
-    def __init__(self, target_bucket, source):
+    def __init__(self, target_bucket,):
         """
         Initializes the S3RequestHandler with the target S3 bucket.
 
@@ -15,7 +15,7 @@ class S3RequestHandler:
         """
         self.s3_client = boto3.client('s3')
         self.target_bucket = target_bucket
-        self.source = source
+
 
     def handle_request(self, request_type, body, key):
         """
@@ -28,38 +28,40 @@ class S3RequestHandler:
         """
         if request_type:
             logging.info(request_type)
+            print(request_type)
             if request_type == 'create':
-                self.create_object(body)
+                self.create_object(body, key)
             elif request_type == 'delete':
                 self.delete_object(key)
             elif request_type == 'update':
-                self.update_object(body)
+                self.update_object(body,key)
             else:
                 logging.error(f"Unknown request type: {request_type}")
+                print(f"Unknown request type: {request_type}")
 
 
 
-    def create_object(self, body):
+    def create_object(self, body, key):
         """Create a new object in the target S3 bucket."""
-        widget_id = body.get('widgetId')
-        owner = body.get('owner', None)
-        if widget_id:
-            if not owner:
-                owner = "unidentified"
-            target_key = f'widgets/{owner}/{widget_id}'
-            self.s3_client.put_object(Bucket=self.target_bucket, Key=target_key, Body=json.dumps(body))
-            logging.info(f'Created object at {target_key}')
-        else:
-            logging.error("Invalid data for creating object.")
+        self.s3_client.put_object(Bucket=self.target_bucket, Key=key, Body=json.dumps(body))
+        logging.info(f"Object created: {self.target_bucket}{key}")
+        print(f"Object created: {self.target_bucket}{key}")
+
 
     def delete_object(self, key):
         """Delete an object from the target S3 bucket."""
-        logging.error(f'Delete logic not yet implemented')
+        try:
+            self.s3_client.delete_object(Bucket=self.target_bucket, Key=key)
+            logging.info(f'Deleted object {key} from bucket {self.target_bucket}')
+            print(f"Deleted object {key} from bucket {self.target_bucket}")
+        except self.s3_client.exceptions.NoSuchKey:
+            logging.warning(f'Object {key} does not exist in bucket {self.target_bucket}. Nothing to delete.')
+            print(f'Object {key} does not exist in bucket {self.target_bucket}. Nothing to delete.')
 
-    def update_object(self, body):
+    def update_object(self, body, key):
         """Placeholder for updating an object in the target S3 bucket."""
         logging.error("Update logic not yet implemented.")
-
+        print("Update logic not yet implemented.")
     def process_requests_from_s3(self,source_bucket):
         """
         Continuously process requests from the source bucket until a stop condition is met.
@@ -77,8 +79,17 @@ class S3RequestHandler:
                 request_type, body, key = s3request_receiver.get_smallest_object()
 
                 if key:
-                    # Process the request
-                    self.handle_request(request_type, body, key)
+                    json_data = json.loads(body)
+                    request_type = json_data.pop('type', None)
+
+                    """Create a new object in the target S3 bucket."""
+                    widget_id = json_data.get('widgetId', None)
+                    owner = json_data.get('owner', None)
+                    if widget_id:
+                        if not owner:
+                            owner = "unidentified"
+                        target_key = f'widgets/{owner}/{widget_id}'
+                    self.handle_request(request_type, body, target_key)
                     s3request_receiver.s3_client.delete_object(Bucket=source_bucket, Key=key)
 
                     # Reset cumulative wait time since a key was found
@@ -89,32 +100,51 @@ class S3RequestHandler:
                     cumulative_wait_time += 0.1  # Increment cumulative wait time
 
             logging.info("No new requests for 5 seconds. Stopping processing.")
+            print("No new requests for 5 seconds. Stopping processing.")
 
+    def SQS_message_delete(self, sqsreceiver, source_queueurl, receipt_handle):
+            sqsreceiver.sqs_client.delete_message(QueueUrl=source_queueurl,ReceiptHandle=receipt_handle)
 
-        def process_request_from_queue(self,source_queueurl):
-            if source_queueurl:
-                SQSrequest_receiver = SQSRequestReceiver(source_queueurl)
+    def process_request_from_queue(self, source_queueurl):
+        if source_queueurl:
+            SQSrequest_receiver = SQSRequestReceiver(source_queueurl)
 
-                cumulative_wait_time = 0  # Initialize cumulative wait time
+            cumulative_wait_time = 0  # Initialize cumulative wait time
 
-                while cumulative_wait_time < 5:  # Continue until 5 seconds of cumulative wait time
-                    # Try to get a request
-                    messages = SQSrequest_receiver.retrieve_messages_from_queue()
+            while cumulative_wait_time < 5:  # Continue until 5 seconds of cumulative wait time
+                # Try to get a request
+                messages = SQSrequest_receiver.retrieve_messages_from_queue(maxno=10)
+                if messages:
+                    for message in messages:
+                        message_body = message['Body']
+                        receipt_handle = message['ReceiptHandle']
+                        message_id = message['MessageId']
+                        json_data = json.loads(message_body)
+                        request_type = json_data.pop('type', None)
 
-                    if key:
-                        # Process the request
-                        self.handle_request(request_type, body, key)
-                        request_receiver.s3_client.delete_object(Bucket=source_bucket, Key=key)
-
-                        # Reset cumulative wait time since a key was found
+                        """Create a new object in the target S3 bucket."""
+                        widget_id = json_data.get('widgetId', None)
+                        owner = json_data.get('owner', None)
+                        if widget_id:
+                            if not owner:
+                                owner = "unidentified"
+                            target_key = f'widgets/{owner}/{widget_id}'
+                            self.handle_request(request_type,json_data, target_key)
+                        else:
+                            logging.error("Invalid data for creating object.")
+                            print("Invalid data for creating object.")
+                        self.SQS_message_delete(SQSrequest_receiver, source_queueurl,receipt_handle)
                         cumulative_wait_time = 0
-                    else:
-                        # Wait for a while before checking again
-                        time.sleep(0.1)  # Wait for 100 ms
-                        cumulative_wait_time += 0.1  # Increment cumulative wait time
 
-                logging.info("No new requests for 5 seconds. Stopping processing.")
+                else:
+                    # Wait for a while before checking again
+                    time.sleep(0.1)  # Wait for 100 ms
+                    cumulative_wait_time += 0.1  # Increment cumulative wait time
+
+            logging.info("No new requests for 5 seconds. Stopping processing.")
+            print("No new requests for 5 seconds. Stopping processing")
 
 
+S3RequestHandler('usu-cs5250-sajan-web').process_request_from_queue('https://sqs.us-east-1.amazonaws.com/186579595491/cs5250-requests')
 
 
--- SQSRequestReceiver.py
+++ SQSRequestReceiver.py
@@ -3,11 +3,11 @@ import botocore
 
 class SQSRequestReceiver:
 
-    def __init__(self):
+    def __init__(self, url):
         self.sqs_client = boto3.client('sqs')
-        self.url = 'https://sqs.us-east-1.amazonaws.com/186579595491/cs5250-requests'
+        self.url = url
 
-    def retrieve_messages_from_queue(self,maxno=10):
+    def retrieve_messages_from_queue(self,maxno=20):
         try:
             response = self.sqs_client.receive_message(
             QueueUrl=self.url,
@@ -15,16 +15,17 @@ class SQSRequestReceiver:
             VisibilityTimeout=60,
             WaitTimeSeconds=10
         )
-            messages = response.get('Messages', [])
-            print(messages)
+            messages = response.get('Messages', None)
+            if not messages:
+                print("No messages")
+            for message in messages:
+                print(message)
+                print("\n")
             return messages
         except botocore.exceptions.ClientError as e:
             print("Something went wrong")
             print(e)
             exit()
 
-
-
-sqs= SQSRequestReceiver()
-print(sqs.retrieve_messages_from_queue())
+sqs = SQSRequestReceiver('https://sqs.us-east-1.amazonaws.com/186579595491/cs5250-requests').retrieve_messages_from_queue(10)
 
